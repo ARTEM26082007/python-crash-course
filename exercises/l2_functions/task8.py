fe6ad43211ebec4d675a108054eb4c9f7bc691b1
ARTEM26082007 <kapustiankachernobul@gmail.com>
2025-01-31 11:36:14 +0200

    Update task8.py

--- exercises/l2_functions/task8.py
+++ exercises/l2_functions/task8.py
@@ -1,13 +1,12 @@
 
-# Finish the function `is_even` to fix the script the
-# way it works without errors
-def is_even(n: int) -> bool:
-    pass
+# Declare the function named `greet` and write its body
+# the way that it fixes all errors in the script
 
+def greet(name: str) -> str:
+    return f"Hello, {name}!"
 
 # Do not change the below's code
 if __name__ == "__main__":
-    assert is_even(2) is True
-    assert is_even(1) is False
-    assert is_even(3) is False
-    assert is_even(4) is True
+    assert greet("Jon") == "Hello, Jon!"
+    assert greet("Alice") == "Hello, Alice!"
+    assert greet("Baba") == "Hello, Baba!"
