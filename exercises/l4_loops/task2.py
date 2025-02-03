19b5cd0ff4f893198464666ebbe1651d38e3f2b8
ARTEM26082007 <kapustiankachernobul@gmail.com>
2025-02-03 17:41:45 +0200

    Update task2.py

--- exercises/l4_loops/task2.py
+++ exercises/l4_loops/task2.py
@@ -7,8 +7,10 @@ def odd_str(n: int) -> str:
     #
     # For example,
     # (0, 1, 2, 3, 4) -> "13"
-    # (0, 1, 2, 3, 4, 5, 6) -> "135" 
+    # (0, 1, 2, 3, 4, 5, 6) -> "135"
 
+    for i in range(1, n + 1, 2):
+        s += str(i)
     return s
 
 
